01615f83d653de421b296da3a3ad31f0bee7373e
Amrit Raj <cnamritraj9123@gmail.com>
2026-07-13 11:48:31 +0000

    conversion

--- Basic_Js/conversionOperation.js
+++ Basic_Js/conversionOperation.js
@@ -22,3 +22,50 @@ let bolleanIsLoggedIn = Boolean(isLoggedIn)
     null = undefined => false
 */
 
+
+//   ********* Operations ********
+
+let value =4
+let negValue= -value
+// console.log(negValue)
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+*/
+
+/*
+let str1="hello"
+let str2=" amrit"
+let str=str1+str2
+console.log(str);
+*/
+
+/*
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+*/
+
+// console.log(+true);  // 1
+
+let gameCounter=100
+console.log(++gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
