ae4a2d94083ad67d49fd878a55fb622f25a806c1
Amrit Raj <cnamritraj9123@gmail.com>
2026-07-13 11:28:35 +0000

    conversion of datatypes

---
+++ Basic_Js/conversionOperation.js
@@ -0,0 +1,24 @@
+let  score=88;
+// console.log(typeof score);
+
+let valueInNumber =Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber)
+
+/* "88" => 88
+    "88hdf"=> NaN
+    null = false => 0 || true => 1
+    undefind=>NaN
+*/
+
+
+let isLoggedIn=5;
+let bolleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(bolleanIsLoggedIn);
+
+/* "" => false
+    2 => true
+    "am"=> true
+    null = undefined => false
+*/
+
