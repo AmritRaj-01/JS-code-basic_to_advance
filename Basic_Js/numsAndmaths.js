b01d27e7df050711d2fb8c454d632f11a4e04f3d
Amrit Raj <cnamritraj9123@gmail.com>
2026-07-16 10:42:39 +0000

    math and numbers

---
+++ Basic_Js/numsAndmaths.js
@@ -0,0 +1,39 @@
+const num1=122;
+const score=new Number (2323);
+// console.log(score);
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(2));  // give decimal after number 
+
+// const num=13.4
+// console.log(num.toPrecision(2));
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));  // give comma according to indian standerd
+
+
+
+/* ******** MATHS   *********   */
+// console.log(Math);
+// console.log(Math.abs(-3));   // 3
+// console.log(Math.sqrt(4));    // 2 
+// console.log(Math.round(4.3));   // 4 
+// console.log(Math.ceil(4.3));   // 5 always take upper value
+// console.log(Math.floor(4.3));   // 4  always take lower value 
+
+// console.log(Math.min(2,4,2,1,4,4,6));
+// console.log(Math.max(2,4,2,1,4,4,6));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
