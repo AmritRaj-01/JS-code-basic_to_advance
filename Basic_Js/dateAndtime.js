f6bb4d4d5fb89597be6372e52bbcf62c3e134a9f
Amrit Raj <cnamritraj9123@gmail.com>
2026-07-16 11:10:46 +0000

    date and times

---
+++ Basic_Js/dateAndtime.js
@@ -0,0 +1,40 @@
+// Dates
+
+let mydate=new Date();
+// console.log(mydate);
+
+// console.log(mydate.toString());  //Thu Jul 16 2026 10:50:32 GMT+0000 (Coordinated Universal Time)
+
+// console.log(mydate.toDateString());  // Thu Jul 16 2026
+
+// console.log(mydate.toISOString());    // 2026-07-16T10:50:32.167Z
+// console.log(mydate.toJSON());    // 2026-07-16T10:50:32.167Z
+
+// console.log(mydate.toLocaleString());  // 7/16/2026, 10:53:28 AM
+// console.log(typeof mydate);   // object
+
+// let myCreateDate=new Date()  // 2026-07-16T10:54:37.953Z
+// let myCreateDate1=new Date(2006,8,18)  
+let mycreatedDate=new Date("2026-07-16");
+// console.log(myCreateDate1.toDateString());  // Mon Sep 18 2006
+
+
+let timestamp=Date.now();
+// console.log(timestamp);
+// console.log(mycreatedDate.getTime());
+
+let newDate=new Date();
+// console.log(newDate);
+
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getHours());
+// console.log(newDate.getMonth());
+
+
+
+
+
+
+
