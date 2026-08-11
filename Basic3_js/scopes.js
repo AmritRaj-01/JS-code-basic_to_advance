d9f990a47fb5cfbdf923f4b6fb1680018cc749b0
Amrit Raj <cnamritraj9123@gmail.com>
2026-08-11 18:32:15 +0000

    scopes_basic

---
+++ Basic3_js/scopes.js
@@ -0,0 +1,16 @@
+          //  global scope 
+// let num=5;
+// const num1=120;
+// var num2=23;
+
+if(true){      // in this if block global variable are used without any error 
+  let num=50;     // block scope ->they are not used outside from this if block  
+  const num1=12;    
+  var num2=230;
+}
+
+// console.log(num);
+// console.log(num1);
+// console.log(num2);
+
+
